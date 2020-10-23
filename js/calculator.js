987a8d9306e6485c6596103271c961a533947fdf
MattChorba-codemaster <cho20006@byui.edu>
2020-10-23 16:07:49 -0400

    Simple Calc w06

---
+++ js/calculator.js
@@ -0,0 +1,42 @@
+function doMath()  {
+
+    // Input
+    // grab input strings from user and assign them as the value for four declared variables
+let number1  = parseFloat(document.getElementById("number1").value);
+let number2  = parseFloat(document.getElementById("number2").value);
+let operator = document.getElementById("operator").value;
+let answer   = parseFloat(document.getElementById("answer").value);
+let correctAnswer;
+
+// Processing
+// match the operator to the correct math and evaluate the correctness of the user answer
+switch (operator) {
+    case '+':
+        correctAnswer = number1 + number2;
+        break;
+
+    case '-':
+        correctAnswer = number1 - number2;
+        break;
+
+    case '*':
+        correctAnswer = number1 * number2;
+        break;
+        
+    case '/':
+        correctAnswer = number1 / number2;
+        break;    
+
+    default:
+        alert("Use '+' for addition, '-' for subtraction, '*' for multiplication, and '/' for division.")
+        break;
+}
+
+// Output
+if(answer == correctAnswer)
+    {
+    document.getElementById("outputEvaluation").innerHTML = "Correct!  Good job.";
+    }   else
+    document.getElementById("outputEvaluation").innerHTML = "Incorrect.  Try again!";
+
+}
