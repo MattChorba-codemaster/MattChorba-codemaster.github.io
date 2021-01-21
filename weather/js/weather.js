7b0433e33e504c09984935a473b0bb5b90fbea9b
MattChorba-codemaster <cho20006@byui.edu>
2021-01-20 23:00:33 -0500

    preston wk3 updates

--- weather/js/weather.js
+++ weather/js/weather.js
@@ -1,11 +1,17 @@
 var d = new Date();
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 document.getElementById("year").innerHTML = '&copy;' + d.getFullYear() + '&#124; Matthew Anthony Chorba &#124; Ohio &#124; <a id="byuiLink" href="https://www.byui.edu/online">BYUI Online Learning</a>';
 
 let oLastModif = new Date(document.todayDate);
 document.getElementById("todayDate").innerHTML = days[d.getDay()] + ', ' + d.getDate() +' ' + months[d.getMonth()] + ' ' + d.getFullYear();
+
+document.getElementById("today").innerHTML = days[d.getDay()];
+document.getElementById("todayPlusOne").innerHTML = days[d.getDay() + 1];
+document.getElementById("todayPlusTwo").innerHTML = days[d.getDay() + 2];
+document.getElementById("todayPlusThree").innerHTML = days[d.getDay() + 3];
+document.getElementById("todayPlusFour").innerHTML = days[d.getDay() + 4];
 
 function toggleMenu() {
     document
