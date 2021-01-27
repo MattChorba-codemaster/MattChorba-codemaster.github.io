bbce91960132e9f3cc382e2439b6f4ff2dc3b900
MattChorba-codemaster <cho20006@byui.edu>
2021-01-26 22:47:03 -0500

    wind chill fix

--- weather-w03-part-2/js/windchill.js
+++ weather-w03-part-2/js/windchill.js
@@ -1,14 +1,15 @@
-function windChill() {
-    var s = parseFloat(document.getElementById("windSpeed").innerHTML);
-    var t = parseFloat(document.getElementById("highTemp").innerHTML);
-    var f = 35.74 + (0.6215 * t) - (35.75 * (pow(s, 0.16))) + (0.4275 * t * (pow(s, 0.16)));
+var t = parseFloat(document.getElementById("highTemp").innerHTML);
 
-    // Output
-    document.getElementById("windChillValue").innerHTML = "N/A";
+var s = parseFloat(document.getElementById("windSpeed").innerHTML);
 
-    if((t <= 50) && (s >= 3.0))
-    {
-    document.getElementById("windChillValue").innerHTML = f;
-    } 
+var expVal = Math.pow(s, 0.16);
 
-}
+document.getElementById("windChillValue").innerHTML = "N/A"
+
+var f = 35.74 + (0.6215 * t) - (35.75 * expVal) + (0.4275 * t * expVal);
+
+// Output
+if((t <= 50) && (s >= 3.0))
+{
+    document.getElementById("windChillValue").innerHTML = f.toFixed(0);
+} 
