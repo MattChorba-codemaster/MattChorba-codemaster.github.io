4ec1a992acc672ff3644adda3b2f06eba3e55869
MattChorba-codemaster <cho20006@byui.edu>
2021-02-12 18:44:07 -0500

    weather final

---
+++ lesson-6/js/weatherapi.js
@@ -0,0 +1,14 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=d135288a5a341cde16f7e7dd0c5ed6c3';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+  });
